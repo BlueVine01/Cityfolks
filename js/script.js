a4fe85aa49583c8eb2d45629bbf91b4b1c81515f
BlueVine01 <89752977+BlueVine01@users.noreply.github.com>
2024-01-29 04:05:04 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 ///////////////////////////////////
 ///////////////////////////////////
+//////////////ACTIVE NAVBAR
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav ul li a');
+
+window.onscroll = () => {
+    sections.forEach(aa => {
+        let top = window.scrollY;
+        let offset = aa.offsetTop - 150;
+        let height = aa.offsetHeight;
+        let id = aa.getAttribute('id')
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a[href*= ' + id +']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+
+///////////////////////////////////
+///////////////////////////////////
 //////////////COUNTDOWN TIMEOUT
 
 var countDownDate = new Date("Feb 15, 2024 00:00:00").getTime();
